524276d3b159c522b50ffba818ebbcecc82a345a
Ryan Barnes <barnesry@juniper.net>
2016-11-29 10:03:31 -0800

    utilization and presentation  updates

--- python/vc-topology.py
+++ python/vc-topology.py
@@ -10,9 +10,36 @@
 # Version History
 #   0.1     Nov 29, 2016
 
+# barnesry-mbp:python barnesry$ ./vc-topology.py
+# Connecting to...192.168.57.100
+# Success!
+# Members : 2
+# SUMMARY
+# id    serial_num      model                role
+# ----------------------------------------
+# 0     CT0213051609    ex2200-48p-4g        Master*
+# 1     GP0214373311    ex2200-c-12t-2g      Linecard
+
+# Back Plane Utilization Statistics
+# ---------------------------------------------
+
+# Member0 of type ex2200-48p-4g connected to:
+# Member1 via vcp-255/0/46   	 UtilIn:0% UtilOut:0%
+# Member1 via vcp-255/0/47   	 UtilIn:0% UtilOut:0%
+
+# Member1 of type ex2200-c-12t-2g connected to:
+# Member0 via vcp-255/1/0    	 UtilIn:0% UtilOut:0%
+# Member0 via vcp-255/1/1    	 UtilIn:0% UtilOut:0%
+
+
 from jnpr.junos import Device
 from lxml import etree as etree
-from collections import defaultdict
+from collections import defaultdict, OrderedDict
+
+HOST = '192.168.57.100'
+USER = 'root'
+PASS = 'junos123'
+
 
 class VCMemberSwitch(object):
     """ Describes a VC Member Switch in a stack """
@@ -31,15 +58,28 @@ class VCMemberSwitch(object):
 
     @classmethod
     def find_by_id(cls, id):
+        """ Returns a list containing switch object by ID """
         return VCMemberSwitch.id_index[id]
 
     def add_neighbor(self, VCInterface):
         self.neighbor_list.append(VCInterface)
 
     def describe_neighbors(self):
+        neighbors = {}
         for neighbor in self.neighbor_list:
             remote_switch = VCMemberSwitch.id_index[neighbor.remote_switch_id][0]
-            print remote_switch.serial_number + ":" + neighbor.local_vc_port_name
+            neighbors[remote_switch.id]
+            remote_sw
+            print remote_switch.id + " via " + neighbor.local_vc_port_name
+
+    def describe_vcp_ports(self):
+        """ Returns list of local vcp ports and associated neighbors """
+        ports = {}
+        for neighbor in self.neighbor_list:
+            ports[neighbor.local_vc_port_name] = {
+                'remote_switch_id' : neighbor.remote_switch_id
+            }
+        return ports
 
 class VCInterface(object):
     """ Describes a VC interface on a VC member """
@@ -53,6 +93,16 @@ class VCInterface(object):
         """ Returns the VCMemberSwitch attached to this interface """
         for switch in VCMemberSwitch.find_by_id(self.local_switch_id):
             print "Parent Serial : {}".format(switch.serial_number)
+
+    def get_stats(self, interface_name):
+        """ Returns stats for a given vc_port
+
+            Input : VCInterface object
+            Output : VCInterface object
+        """
+        pass    # not sure how to pass connection object in yet
+
+
 
 
 
@@ -69,16 +119,17 @@ def print_summary(member_list):
         #print "Neighbors (id:remote_port_name)"
         #print member.describe_neighbors()
 
+
 def main():
 
-    with Device(host='192.168.57.100', user='root', passwd='junos123') as dev:
+    with Device(host=HOST, user=USER, passwd=PASS) as dev:
         print "Connecting to...{}".format(dev.hostname)
         dev.open()
         print "Success!"
 
         vci = dev.rpc.get_virtual_chassis_information(detail=True)
-        # vci_stats = dev.rpc.get_virtual_chassis_port_statistics()
         # vc = dev.rpc.get_virtual_chassis_device_topology()
+        vci_stats = dev.rpc.get_virtual_chassis_port_statistics()
 
         vc_member_list = []
 
@@ -107,10 +158,32 @@ def main():
         print "Members : {}".format(len(vc_member_list))
         print_summary(vc_member_list)
 
-        # for switch in VCMemberSwitch.find_by_id('1'):
-        #     print "Parent Serial : {}".format(switch.serial_number)
-        #     print "Neighbor List : "
-        #     switch.describe_neighbors()
+        # Report VC Port Utilization for each VC port attached to each VC Member
+        print "\nBack Plane Utilization Statistics"
+        print "-"* 45
+
+        keys = sorted(VCMemberSwitch.id_index.keys())
+        for key in keys:
+            switch = VCMemberSwitch.find_by_id(key)[0]
+        # for index, switch_list in VCMemberSwitch.id_index.iteritems():
+            # switch = switch_list[0]
+            # print "Parent Serial : {}".format(switch.serial_number)
+
+            ports = switch.describe_vcp_ports()
+            print "\nMember{} of type {} connected to:".format(switch.id, switch.model)
+
+            for remote_vc_port_name, v in ports.iteritems():
+                xpath_string = '//statistics-port-list/statistics[port-name[text() = "' + \
+                    remote_vc_port_name + '"]]'
+                result = vci_stats.xpath(xpath_string)
+                util_in = result[0].find('input-utilization').text
+                util_out = result[0].find('output-utilization').text
+                # print etree.tostring(result[0])
+                print "Member{} via {:15}\t UtilIn:{}% UtilOut:{}%".format(v['remote_switch_id'],
+                                                                           remote_vc_port_name,
+                                                                           util_in,
+                                                                           util_out)
+
 
 # executes only if not called as a module
 if __name__ == "__main__":
